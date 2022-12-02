7f01ed05de99014e5cba444844b0f19809c02993
Gedralge <Gedralgerrol@gmail.com>
2022-12-02 12:17:06 -0800

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import ethan
+    import sys
     result = 0
-    for i in range(1, len(ethan.argv)):
-        result += int(ethan.argv[i])
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
         print("{}".format(result))
