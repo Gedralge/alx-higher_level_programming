f0f5df3c82bdd3797c182559e6524b6ccecde654
Gedralge <Gedralgerrol@gmail.com>
2022-12-04 10:36:18 -0800

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     """
-    A function thatr finds the biggest integer of a list
+    A function that finds the biggest integer of a list
     """
     if len(my_list) == 0:
         return None
