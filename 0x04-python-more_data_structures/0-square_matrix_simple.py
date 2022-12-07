ae787167f4902c2733ae56d5b77dc6c01770614f
Gedralge <Gedralgerrol@gmail.com>
2022-12-07 09:14:43 -0800

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = [x[:] for x in matrix]
+    for i in range(len(new_matrix[i])):
+        for j in range(len(new_matrix[i])):
+            new_matrix[i][j] = new_matrix[i][j] **
+            return new_matrix
