b1062f1218140cba7ec602420f5ee9ddad1da6bf
Gedralge <Gedralgerrol@gmail.com>
2022-12-09 12:43:23 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = my_list.copy()
-    for n, i in enumerate(new):
-        if i == search:
-            new[n] = replace
-            return new
+    new = []
+    for n in my_list:
+        if n == search:
+            new.append(replace)
+        else:
+            new.append(n)
+            return (new)
