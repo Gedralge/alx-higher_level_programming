4c785d2d7fb74ebdeead9a670e8f840ce1b89bf3
Gedralge <Gedralgerrol@gmail.com>
2022-12-09 13:00:43 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for element in my_list:
-        if element == search:
-            new_list.append(replace)
-        else:
-            new_list.append(element)
-            return new_list
+    return([replace if number == search else number for number in my_list])
