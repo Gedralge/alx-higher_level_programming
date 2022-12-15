07d284da4fa99db802f7efcf113b315de1b4c4d6
Gedralge <Gedralgerrol@gmail.com>
2022-12-15 09:46:04 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        max_value = max(a_dictionary, key=a_dictionary.get)
+    """
+    Returns key with the biggest integer value
+    """
+    if a_dictionary is None or a_dictionary is {}:
+        return None
     else:
-        max_value = None
-        return(max_value)
+        return max(a_dictionary)
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,4 +3,4 @@ def multiply_by_2(a_dictionary):
     """
     Returns a new dictionary with all values
     """
-    return {k: v*2 for k, v in a_dictionary.
+    return {k: v*2 for k,  v, in a_dictionary.
