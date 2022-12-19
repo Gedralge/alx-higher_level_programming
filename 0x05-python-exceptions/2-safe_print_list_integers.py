0d2a1f0f09b0316e12b6ad7ef5386628fa7bb0a8
Gedralge <Gedralgerrol@gmail.com>
2022-12-19 14:53:32 -0800

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+ def safe_print_list_integers(my_list=[], x=0):
+     count = 0
+     try:
+         for i in range(X):
+             if insistance(ny_list[i], int):
+                 print("{:d}".format(my_list[i]), end='')
+                 count += 1
+                 print()
+     except:
+         pass
+     return count
