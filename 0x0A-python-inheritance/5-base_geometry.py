f25243bc713d133d4ac65c28efcefd2d9fbaf481
Gedralge <Gedralgerror@mail.com>
2023-01-09 12:32:01 -0800

    5-base_geometry.py

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
-"""
-module for BaseGeometry
-"""
+"""star a new class called BaseGeometry"""
 
 
-class  BaseGeometry:
+class BaseGeometry:
     """An empty class"""
     pass
