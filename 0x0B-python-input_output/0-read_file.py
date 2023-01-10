d0999f2c5326e7c07d8791eddc7253c089ac3adf
Gedralge <Gedralgerror@mail.com>
2023-01-10 03:07:21 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""This defines read_file"""
+""" This defines read_file """
 
 
 def read_file(filename=""):
-    """reads from a text file passed in as input"""
+    """ reads from a text file passed in as input """
     if filename:
         with open(filename, "r", encoding="utf-8") as f:
             text = f.read()
